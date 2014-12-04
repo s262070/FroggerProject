fe5d6876afefc09d35a20745f8521b796aeaff47
s262070 <swatsmartkid@yahoo.com>
2014-12-04 15:17:16 -0600

    road was made

--- FroggerProj.py
+++ FroggerProj.py
@@ -1,15 +1,17 @@
 from Tkinter import *
 root = Tk()
-drawpad = Canvas(root, width=800,height=600, background='gray')
+drawpad = Canvas(root, width=800,height=600, background='blue')
 player = drawpad.create_oval(390,580,410,600, fill="blue")
-car1 = drawpad.create_rectangle(50,50,100,60, fill="red")
-car2 = drawpad.create_rectangle(50,50,100,60, fill="red")
+Grass = drawpad.create_rectangle(0,350,800,600, fill="green")
+Road = drawpad.create_rectangle(0,425,800,525, fill="grey")
+car1 = drawpad.create_rectangle(350,465,400,490, fill="red")
+car2 = drawpad.create_rectangle(350,435,400,460, fill="red")
 
 
 
-direction = 5
-direction = -5
-fast = 7
+direction = 3
+direction = -3
+fast = 3
 
 
 class myApp(object):
@@ -33,16 +35,16 @@ class myApp(object):
         px1,py1,px2,py2 = drawpad.coords(player)
 
         if x2 > 800:
-            direction = - 5
+            direction = - 3
         elif x1 < 0:
-            direction = 5
+            direction = 3
         drawpad.move(car1, direction, 0)
         drawpad.after(5,self.animate)
         x1,y1,x2,y2 = drawpad.coords(car2)
         if x2 > 800:
-            fast = - 7
+            fast = - 3
         elif x1 < 0:
-            fast = 7
+            fast = 3
         drawpad.move(car2, fast, 0)
  
             
@@ -51,13 +53,13 @@ class myApp(object):
         global drawpad
         global player
         if event.char == "w":
-            drawpad.move(player,0,-4)
+            drawpad.move(player,0,-3)
         if event.char == "s":
-            drawpad.move(player,0,4)
+            drawpad.move(player,0,3)
         if event.char == "d":
-                drawpad.move(player,4,0)
+                drawpad.move(player,3,0)
         if event.char == "a":
-            drawpad.move(player,-4,0)
+            drawpad.move(player,-3,0)
 
             
     
